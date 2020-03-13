cd54338785261879778c36af56b2f24bb764459c
gitName <a@b.c>
2020-03-13 13:02:37 -0300

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+var launchTime = new Date("03/30/2020 10:00").getTime();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var currentTime = new Date().getTime();
+  var numberOfMilliseconds = parseInt(launchTime - currentTime);
+  var numberOfDays = parseInt(numberOfMilliseconds/(24 * 3600 * 1000));
+  res.render('index', { title: 'CONTAGEM REGRESSIVA', countDown: numberOfDays });
+});
+
+module.exports = router;
